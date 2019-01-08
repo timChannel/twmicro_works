a1da50bab18da49edaad2e47b4282ccca52a59ad
twmicro <chicherintim@gmail.com>
2019-01-08 19:27:26 +0300

    added beta version of Pysslaformer

---
+++ Pysslaformer 1.1.0 beta/Pysslaformer/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Pysslaformer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SwitchSize = new System.Windows.Forms.Button();
+            this.ToTray = new System.Windows.Forms.Button();
+            this.SetColors = new System.Windows.Forms.Button();
+            this.SetSize = new System.Windows.Forms.Button();
+            this.xDelta = new System.Windows.Forms.NumericUpDown();
+            this.yDelta = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xDelta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yDelta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pysslaformer";
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Red;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit.Location = new System.Drawing.Point(628, 12);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(27, 25);
+            this.Exit.TabIndex = 1;
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // SwitchSize
+            // 
+            this.SwitchSize.BackColor = System.Drawing.Color.Yellow;
+            this.SwitchSize.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SwitchSize.Location = new System.Drawing.Point(595, 12);
+            this.SwitchSize.Name = "SwitchSize";
+            this.SwitchSize.Size = new System.Drawing.Size(27, 25);
+            this.SwitchSize.TabIndex = 2;
+            this.SwitchSize.UseVisualStyleBackColor = false;
+            this.SwitchSize.Click += new System.EventHandler(this.SwitchSize_Click);
+            // 
+            // ToTray
+            // 
+            this.ToTray.BackColor = System.Drawing.Color.Lime;
+            this.ToTray.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ToTray.Location = new System.Drawing.Point(562, 12);
+            this.ToTray.Name = "ToTray";
+            this.ToTray.Size = new System.Drawing.Size(27, 25);
+            this.ToTray.TabIndex = 3;
+            this.ToTray.UseVisualStyleBackColor = false;
+            this.ToTray.Click += new System.EventHandler(this.ToTray_Click);
+            // 
+            // SetColors
+            // 
+            this.SetColors.Location = new System.Drawing.Point(17, 62);
+            this.SetColors.Name = "SetColors";
+            this.SetColors.Size = new System.Drawing.Size(113, 36);
+            this.SetColors.TabIndex = 4;
+            this.SetColors.Text = "Set 2 colors";
+            this.SetColors.UseVisualStyleBackColor = true;
+            this.SetColors.Click += new System.EventHandler(this.SetColors_Click);
+            // 
+            // SetSize
+            // 
+            this.SetSize.Location = new System.Drawing.Point(17, 134);
+            this.SetSize.Name = "SetSize";
+            this.SetSize.Size = new System.Drawing.Size(113, 36);
+            this.SetSize.TabIndex = 5;
+            this.SetSize.Text = "Set size";
+            this.SetSize.UseVisualStyleBackColor = true;
+            this.SetSize.Click += new System.EventHandler(this.SetSize_Click);
+            // 
+            // xDelta
+            // 
+            this.xDelta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xDelta.Location = new System.Drawing.Point(77, 104);
+            this.xDelta.Name = "xDelta";
+            this.xDelta.Size = new System.Drawing.Size(53, 24);
+            this.xDelta.TabIndex = 6;
+            // 
+            // yDelta
+            // 
+            this.yDelta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yDelta.Location = new System.Drawing.Point(17, 104);
+            this.yDelta.Name = "yDelta";
+            this.yDelta.Size = new System.Drawing.Size(53, 24);
+            this.yDelta.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(155, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Edit ForeColor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 589);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.yDelta);
+            this.Controls.Add(this.xDelta);
+            this.Controls.Add(this.SetSize);
+            this.Controls.Add(this.SetColors);
+            this.Controls.Add(this.ToTray);
+            this.Controls.Add(this.SwitchSize);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.xDelta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yDelta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button SwitchSize;
+        private System.Windows.Forms.Button ToTray;
+        private System.Windows.Forms.Button SetColors;
+        private System.Windows.Forms.Button SetSize;
+        private System.Windows.Forms.NumericUpDown xDelta;
+        private System.Windows.Forms.NumericUpDown yDelta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Pysslaformer 1.1.0 beta/Pysslaformer/MainForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pysslaformer
+{
+    public partial class MainForm : Form
+    {
+        int MatrixWidth = 100;
+        int MatrixHeight = 100;
+        Color BackgroundColor = Color.Black;
+        Color ForegroundColor = Color.White;
+        List<GraphicsPath> Matrix = new List<GraphicsPath>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SwitchSize_Click(object sender, EventArgs e)
+        {
+            WindowState = WindowState == FormWindowState.Normal ? WindowState = FormWindowState.Maximized : FormWindowState.Normal;
+        }
+
+        private void ToTray_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void SetColors_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                BackgroundColor = colorDialog.Color;
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ForegroundColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void SetSize_Click(object sender, EventArgs e)
+        {
+            MatrixWidth = (int)xDelta.Value * 10;
+            MatrixHeight = (int)yDelta.Value * 10;
+            GenerateMatrix();
+        }
+
+        private void GenerateMatrix()
+        {
+            int CircleSize = 10;
+            int Circles = (MatrixWidth / CircleSize) * (MatrixHeight / CircleSize);
+            int CirclesX = MatrixWidth / CircleSize;
+            int CirclesY = MatrixHeight / CircleSize;
+            int CurrentCol = 1;
+            int CurrentRow = 1;
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            for (int i = 0; i < Circles; i++)
+            {
+                if (i % CirclesX == 0 && i != 1) { CurrentCol++; CurrentRow = 1; }
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(CurrentRow * CircleSize, CurrentCol * CircleSize, CircleSize, CircleSize);
+                g.FillPath(new SolidBrush(BackgroundColor), path);
+                Matrix.Add(path);
+                CurrentRow++;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            if (e.Button == MouseButtons.Left)
+            {
+                for (int i = 0; i < Matrix.Count; i++)
+                {
+                    if (Matrix[i].IsVisible(e.X, e.Y))
+                    {
+                        g.FillPath(new SolidBrush(ForegroundColor), Matrix[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ForegroundColor = dialog.Color;
+            }
+        }
+    }
+}
